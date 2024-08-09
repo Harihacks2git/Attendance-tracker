40d6dfb0e62c738db9436335c886d160e6e4e89e
Harihacks2git <harihacksofficial@gmail.com>
2024-08-09 17:24:09 +0530

    Changed dbcredentials

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost','root','','checkmate_wt');
+    $conn = new mysqli('sql312.infinityfree.com','if0_37046410','WXulzTiwRja','if0_37046410_checkmate');
     if ($conn->connect_error) {
         die('Error connecting to database'. $conn->connect_error);
     }
